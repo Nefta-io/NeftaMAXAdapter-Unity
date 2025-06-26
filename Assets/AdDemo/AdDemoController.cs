6658426e4d547ab455245d01377deb2820180643
Tomaz Treven <treven@nefta.io>
2025-06-26 10:20:03 +0200

    Added code for disabling b2b ad loading in MAX

--- Assets/AdDemo/AdDemoController.cs
+++ Assets/AdDemo/AdDemoController.cs
@@ -12,11 +12,33 @@ namespace AdDemo
 
 #if UNITY_IOS
         private const string NeftaId = "5661184053215232";
+
+        private readonly string[] _adUnits = new string[] {
+            // interstitials
+            "6d318f954e2630a8",
+            "37146915dc4c7740",
+            "e5dc3548d4a0913f",
+            // rewarded
+            "918acf84edf9c034",
+            "37163b1a07c4aaa0",
+            "e0b0d20088d60ec5"
+        };
         
         [DllImport("__Internal")]
         private static extern void CheckTrackingPermission();
 #else // UNITY_ANDROID
         private const string NeftaId = "5643649824063488";
+
+        private readonly string[] _adUnits = new string[] {
+            // interstitials
+            "7267e7f4187b95b2",
+            "00b665eda2658439",
+            "87f1b4837da231e5",
+            // rewarded
+            "72458470d47ee781",
+            "5305c7824f0b5e0a",
+            "a4b93fe91b278c75"
+        };
 #endif
         private bool _isBannerShown;
 
@@ -48,8 +70,8 @@ namespace AdDemo
             {
                 Debug.Log("MAX SDK Initialized");
             };
-                    
-            MaxSdk.SetSdkKey(MaxSdkKey);
+            
+            MaxSdk.SetExtraParameter("disable_b2b_ad_unit_ids", string.Join(",", _adUnits));
             MaxSdk.InitializeSdk();
         }
         
